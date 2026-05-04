82f585ce66a7eaa061ab8657f35755df9b76ba67
Martin <battumartindavid@gmail.com>
2026-05-03 17:37:19 -0700

    add NavItems method to components folder

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Image from "next/image";
+import NavItems from "./NavItems";
 
 const Header = () => {
     return (
@@ -9,7 +9,7 @@ const Header = () => {
                     <Image src="/assets/icons/logo.svg" alt="Vertex logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
                 </Link>
                 <nav className="hidden sm:block">
-                    {/*    NavItems */}
+                    <NavItems />
                 </nav>
                 {/*     UserDropdown */}
             </div>
---
+++ components/NavItems.tsx
@@ -0,0 +1,6 @@
+const NavItems = () => {
+    return (
+        <div>Navitems</div>
+    )
+}
+export default NavItems
