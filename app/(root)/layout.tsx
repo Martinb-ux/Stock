42a437b215c3989687a871ec1915b88d7eb0998e
Martin <battumartindavid@gmail.com>
2026-05-03 17:09:53 -0700

    add header.tsx for header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,37 +1,11 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "../globals.css";
-import { cn } from "lib/utils";
-
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Vertex",
-  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-      <html lang="en" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-      </html>
-  );
+const Layout = ({ children }: {children : React.ReactNode }) => {
+    return (
+        <main className="min-h-screen text-gray-400">
+            {/*Header*/}
+            <div className="container py-10">
+                {children}
+            </div>
+        </main>
+    )
 }
+export default Layout
