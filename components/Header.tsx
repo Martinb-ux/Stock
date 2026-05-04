002dcb9cd083d85320792960c32f33b4fe1e1bca
Martin <battumartindavid@gmail.com>
2026-05-03 17:27:56 -0700

    add nav components to components folder

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Header = () => {
     return (
-        <header>Header</header>
+        <header className="sticky top-0 header">
+            <div className="container header-wrapper">
+                <Link href="/">
+                    <Image src="/assets/icons/logo.svg" alt="Vertex logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
+                </Link>
+                <nav className="hidden sm:block">
+                    {/*    NavItems */}
+                </nav>
+                {/*     UserDropdown */}
+            </div>
+        </header>
     )
 }
 export default Header
