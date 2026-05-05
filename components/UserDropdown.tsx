21a9ca8b5fbc3235d61fbb89866cfb723bf755a8
Martin <battumartindavid@gmail.com>
2026-05-04 23:03:36 -0700

    add dropdown menu imports to userdropdown.tsx

---
+++ components/UserDropdown.tsx
@@ -0,0 +1,15 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+const UserDropdown = () => {
+    return (
+        <div>UserDropdown</div>
+    )
+}
+export default UserDropdown
